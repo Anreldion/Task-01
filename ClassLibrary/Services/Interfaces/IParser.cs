57cf63bce4d974a974339dfe23caa5a2e8722206
asamusenkaEPAM <asamusenka@idbs.com>
2025-06-05 13:39:48 +0200

    Remove parser class and add Parser service

---
+++ ClassLibrary/Services/Interfaces/IParser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ClassLibrary.Products;
+using System.Collections.Generic;
+
+namespace ClassLibrary.Services.Interfaces
+{
+    public interface IParser
+    {
+        string Serialize(BakeryProduct[] input);
+        BakeryProduct[] Deserialize(string input);
+    }
+}
---
+++ ClassLibrary/Services/Parser.cs
@@ -0,0 +1,30 @@
+﻿using ClassLibrary.Services.Interfaces;
+using System.Collections.Generic;
+using System.Text.Json;
+using ClassLibrary.Products;
+
+namespace ClassLibrary.Services
+{
+    public class Parser : IParser
+    {
+        public BakeryProduct[] Deserialize(string input)
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+
+            return JsonSerializer.Deserialize<List<BakeryProduct>>(input, options).ToArray();
+        }
+
+        public string Serialize(BakeryProduct[] input)
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+
+            return JsonSerializer.Serialize(input, options);
+        }
+    }
+}
