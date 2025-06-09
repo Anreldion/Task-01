fd3980614fb5dc4fa503d320c2f22efda4fade1d
asamusenkaEPAM <asamusenka@idbs.com>
2025-06-09 16:56:20 +0200

    Replace internal data to external data, from TestData class

--- Tests/Ingredients/IngredientTest.cs
+++ Tests/Ingredients/IngredientTest.cs
@@ -1,6 +1,7 @@
 ﻿using ClassLibrary.Ingredients;
 using NUnit.Framework;
 using System;
+using Tests.Shared;
 
 namespace Tests.Ingredients;
 
@@ -8,9 +9,6 @@ namespace Tests.Ingredients;
 [TestOf(typeof(Ingredient))]
 public class IngredientTest
 {
-    private const double Tolerance = 0.01;
-    private static Ingredient Flour => new("Flour", calorie: 364, price: 1.50, weight: 0.3);
-    private static Ingredient Water => new("Water", calorie: 0, price: 0, weight: 0.2);
 
     [Test]
     public void Constructor_CallWithoutParameters()
@@ -64,42 +62,42 @@ public class IngredientTest
         var item = new Ingredient(name: "Flour", calorie: 0, price: 1.50, weight: 0.3);
 
         Assert.That(item.Name, Is.EqualTo("Flour"));
-        Assert.That(item.Calorie, Is.EqualTo(0).Within(Tolerance));
-        Assert.That(item.Price, Is.EqualTo(1.50).Within(Tolerance));
-        Assert.That(item.Weight, Is.EqualTo(0.3).Within(Tolerance));
+        Assert.That(item.Calorie, Is.EqualTo(0).Within(TestData.Tolerance));
+        Assert.That(item.Price, Is.EqualTo(1.50).Within(TestData.Tolerance));
+        Assert.That(item.Weight, Is.EqualTo(0.3).Within(TestData.Tolerance));
     }
 
     [Test]
     public void Clone_ReturnsEqualObject()
     {
-        var item = Flour.Clone();
-        Assert.That(item.Equals(Flour));
+        var item = TestData.Flour.Clone();
+        Assert.That(item.Equals(TestData.Flour));
     }
 
     [Test]
     public void Clone_ReturnsDifferentReference()
     {
-        var item = Flour.Clone();
-        Assert.That(item != Flour);
+        var item = TestData.Flour.Clone();
+        Assert.That(item != TestData.Flour);
     }
 
     [Test]
     public void Equals_ReturnsTrue_ForIdenticalValues()
     {
         var item = new Ingredient("Flour", calorie: 364, price: 1.50, weight: 0.3);
-        Assert.That(item.Equals(Flour));
+        Assert.That(item.Equals(TestData.Flour));
     }
 
     [Test]
     public void Equals_ReturnsFalse_ForDifferentValues()
     {
-        Assert.That(!Water.Equals(Flour));
+        Assert.That(!TestData.Water.Equals(TestData.Flour));
     }
 
     [Test]
     public void Equals_ReturnsFalse_WhenComparedWithNull()
     {
-        Assert.That(!Water.Equals(null));
+        Assert.That(!TestData.Water.Equals(null));
     }
 
     [Test]
@@ -113,8 +111,8 @@ public class IngredientTest
     [Test]
     public void GetHashCode_Differs_ForDifferentObjects()
     {
-        var hashCode1 = Water.GetHashCode();
-        var hashCode2 = Flour.GetHashCode();
+        var hashCode1 = TestData.Water.GetHashCode();
+        var hashCode2 = TestData.Flour.GetHashCode();
         Assert.That(hashCode1 != hashCode2);
     }
 
@@ -122,7 +120,7 @@ public class IngredientTest
     [Test]
     public void ToString_ReturnsFormattedString()
     {
-        var item = Water.ToString();
+        var item = TestData.Water.ToString();
         Assert.That(item == "Water (0.2) - 0.00 $ / 0 kcal");
     }
 
